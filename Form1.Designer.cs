bb8f426e7d9505fd8fc6d0acbafebee2ea790c39
Jesus Perez <80136180+JesusPerez10@users.noreply.github.com>
2024-05-24 19:12:10 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Wfdb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPersonajes = new System.Windows.Forms.DataGridView();
+            this.buttonCargaData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxRaza = new System.Windows.Forms.TextBox();
+            this.numericUpDownNivelPoder = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxRaza = new System.Windows.Forms.ComboBox();
+            this.buttonInsertar = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonTestCon = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxHistoria = new System.Windows.Forms.TextBox();
+            this.buttonupdate = new System.Windows.Forms.Button();
+            this.textBoxFechaCreacion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonajes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNivelPoder)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPersonajes
+            // 
+            this.dataGridViewPersonajes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersonajes.Location = new System.Drawing.Point(11, 274);
+            this.dataGridViewPersonajes.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewPersonajes.Name = "dataGridViewPersonajes";
+            this.dataGridViewPersonajes.RowHeadersWidth = 51;
+            this.dataGridViewPersonajes.RowTemplate.Height = 24;
+            this.dataGridViewPersonajes.Size = new System.Drawing.Size(790, 160);
+            this.dataGridViewPersonajes.TabIndex = 0;
+            // 
+            // buttonCargaData
+            // 
+            this.buttonCargaData.Location = new System.Drawing.Point(624, 182);
+            this.buttonCargaData.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCargaData.Name = "buttonCargaData";
+            this.buttonCargaData.Size = new System.Drawing.Size(78, 50);
+            this.buttonCargaData.TabIndex = 1;
+            this.buttonCargaData.Text = "Cargar";
+            this.buttonCargaData.UseVisualStyleBackColor = true;
+            this.buttonCargaData.Click += new System.EventHandler(this.buttonCargaData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 76);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Raza";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 214);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Nivel de Poder";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(154, 24);
+            this.textBoxID.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(49, 20);
+            this.textBoxID.TabIndex = 6;
+            this.textBoxID.Leave += new System.EventHandler(this.textBoxID_Leave);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(110, 48);
+            this.textBoxNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(202, 20);
+            this.textBoxNombre.TabIndex = 7;
+            // 
+            // textBoxRaza
+            // 
+            this.textBoxRaza.Location = new System.Drawing.Point(110, 76);
+            this.textBoxRaza.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxRaza.Name = "textBoxRaza";
+            this.textBoxRaza.Size = new System.Drawing.Size(202, 20);
+            this.textBoxRaza.TabIndex = 8;
+            // 
+            // numericUpDownNivelPoder
+            // 
+            this.numericUpDownNivelPoder.Location = new System.Drawing.Point(163, 207);
+            this.numericUpDownNivelPoder.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDownNivelPoder.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDownNivelPoder.Name = "numericUpDownNivelPoder";
+            this.numericUpDownNivelPoder.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDownNivelPoder.TabIndex = 9;
+            // 
+            // comboBoxRaza
+            // 
+            this.comboBoxRaza.FormattingEnabled = true;
+            this.comboBoxRaza.Location = new System.Drawing.Point(163, 182);
+            this.comboBoxRaza.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxRaza.Name = "comboBoxRaza";
+            this.comboBoxRaza.Size = new System.Drawing.Size(92, 21);
+            this.comboBoxRaza.TabIndex = 10;
+            // 
+            // buttonInsertar
+            // 
+            this.buttonInsertar.Location = new System.Drawing.Point(64, 243);
+            this.buttonInsertar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonInsertar.Name = "buttonInsertar";
+            this.buttonInsertar.Size = new System.Drawing.Size(87, 27);
+            this.buttonInsertar.TabIndex = 11;
+            this.buttonInsertar.Text = "Insertar";
+            this.buttonInsertar.UseVisualStyleBackColor = true;
+            this.buttonInsertar.Click += new System.EventHandler(this.buttonInsertar_Click);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(226, 23);
+            this.buttonBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(56, 19);
+            this.buttonBuscar.TabIndex = 12;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // buttonTestCon
+            // 
+            this.buttonTestCon.Location = new System.Drawing.Point(315, 251);
+            this.buttonTestCon.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTestCon.Name = "buttonTestCon";
+            this.buttonTestCon.Size = new System.Drawing.Size(135, 19);
+            this.buttonTestCon.TabIndex = 13;
+            this.buttonTestCon.Text = "Prueba Conexion";
+            this.buttonTestCon.UseVisualStyleBackColor = true;
+            this.buttonTestCon.Click += new System.EventHandler(this.buttonTestCon_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(61, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Fechacreacion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "historia";
+            // 
+            // textBoxHistoria
+            // 
+            this.textBoxHistoria.Location = new System.Drawing.Point(113, 140);
+            this.textBoxHistoria.Name = "textBoxHistoria";
+            this.textBoxHistoria.Size = new System.Drawing.Size(353, 20);
+            this.textBoxHistoria.TabIndex = 17;
+            // 
+            // buttonupdate
+            // 
+            this.buttonupdate.Location = new System.Drawing.Point(624, 105);
+            this.buttonupdate.Name = "buttonupdate";
+            this.buttonupdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonupdate.TabIndex = 18;
+            this.buttonupdate.Text = "Actualizar";
+            this.buttonupdate.UseVisualStyleBackColor = true;
+            this.buttonupdate.Click += new System.EventHandler(this.buttonupdate_Click);
+            // 
+            // textBoxFechaCreacion
+            // 
+            this.textBoxFechaCreacion.Location = new System.Drawing.Point(145, 105);
+            this.textBoxFechaCreacion.Name = "textBoxFechaCreacion";
+            this.textBoxFechaCreacion.Size = new System.Drawing.Size(296, 20);
+            this.textBoxFechaCreacion.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Wfdb.Properties.Resources.goku;
+            this.ClientSize = new System.Drawing.Size(812, 484);
+            this.Controls.Add(this.textBoxFechaCreacion);
+            this.Controls.Add(this.buttonupdate);
+            this.Controls.Add(this.textBoxHistoria);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonTestCon);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonInsertar);
+            this.Controls.Add(this.comboBoxRaza);
+            this.Controls.Add(this.numericUpDownNivelPoder);
+            this.Controls.Add(this.textBoxRaza);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCargaData);
+            this.Controls.Add(this.dataGridViewPersonajes);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonajes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownNivelPoder)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPersonajes;
+        private System.Windows.Forms.Button buttonCargaData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxRaza;
+        private System.Windows.Forms.NumericUpDown numericUpDownNivelPoder;
+        private System.Windows.Forms.ComboBox comboBoxRaza;
+        private System.Windows.Forms.Button buttonInsertar;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Button buttonTestCon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxHistoria;
+        private System.Windows.Forms.Button buttonupdate;
+        private System.Windows.Forms.TextBox textBoxFechaCreacion;
+    }
+}
+
